777a1b626b5302d893554529c2d509b2ae59f998
Kamaropoulos <kamaropoulos@outlook.com>
2018-05-23 08:24:25 +0300

    Added example code

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Kamaropoulos/goctapus-mongo"
+	"github.com/Kamaropoulos/goctapus-mongo-example/handlers"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	goctapus.Init(os.Args, "debug", "goapp")
+
+	goctapus.File("/", "public/index.html")
+	goctapus.GET("/tasks", handlers.GetTasks())
+	goctapus.PUT("/tasks", handlers.PutTask())
+	goctapus.DELETE("/tasks/:id", handlers.DeleteTask())
+
+	goctapus.Start()
+}
